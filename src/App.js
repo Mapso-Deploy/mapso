a599040d184451252a3fd0c268308185962686ff
Kevin Stephens <kvin.stphns@gmail.com>
2026-07-20 19:44:08 -0400

    Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Landing from "./Landing";
@@ -80,6 +81,7 @@ function App() {
         {/* <NavbarComp/> */}
         <Routes>
           <Route path="/*" element={<ProtectedRoutes />} />
+          <Analytics />
         </Routes>
       </div>
     </Router>
